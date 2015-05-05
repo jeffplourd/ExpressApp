a50f9079d5802861be3d012898f7192e744fd942
Jeff Plourd <jeffplourd@jeffs-mbp.gateway.2wire.net>
2015-05-04 20:45:10 -0700

    Bug fix: replaced 'rgt' with 'rtg'

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use(express.static('public'));
 var redis = require('redis');
 if(process.env.REDISTOGO_URL) {
 	var rtg = require('url').parse(process.env.REDISTOGO_URL);
-	var client = redis.createClient(rgt.port, rgt.hostname);
-	client.auth(rgt.auth.split(':')[1]);
+	var client = redis.createClient(rtg.port, rtg.hostname);
+	client.auth(rtg.auth.split(':')[1]);
 }else {
 	var client = redis.createClient();
 	client.select((process.env.NODE_ENV || 'development').length);
 }
-
+//end redis connection
 
 
 app.get('/cities', function(request, response) {
